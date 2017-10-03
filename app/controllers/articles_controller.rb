705dba1c9723af2add8ce05f1cbe12df14878368
mauricioxpl <mauricio@xplaye.com>
2017-10-03 16:12:06 -0500

    private article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,9 @@ class ArticlesController < ApplicationController
 	  @article.save
 	  redirect_to @article
 	end
+
+	private
+	  def article_params
+	    params.require(:article).permit(:title, :text)
+	  end
 end
